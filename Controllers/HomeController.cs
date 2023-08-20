f5fb17afa67d7192ece8a473e2a9077bef61e2b3
kewang <kewang.h.limbu@gmail.com>
2023-08-20 21:40:11 +0545

    inital commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public async  Task<IActionResult>   GetList()
+        {
+            //List<Class1>? listdata = new List<Class1>();
+            
+            // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+             HttpClient client = new HttpClient();
+
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                //using HttpResponseMessage response = await client.GetAsync("https://restcountries.com/v3.1/all");
+                //response.EnsureSuccessStatusCode();
+                //string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                string responseBody = await client.GetStringAsync("https://restcountries.com/v3.1/all");
+                var listdata = JsonConvert.DeserializeObject<List<Class1>>(responseBody);
+
+                return View(listdata);
+            
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            return View();
+
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
